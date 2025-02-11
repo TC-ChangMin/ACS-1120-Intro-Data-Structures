7bbc85856b7f9799e1c11714f7eeb4a91fd9f198
Nick Nguyen <nickmn2006@gmail.com>
2025-02-11 10:10:19 -0800

    testing new path

--- Code/app.py
+++ Code/app.py
@@ -1,3 +1,8 @@
+import sys
+import os
+
+sys.path.append(os.path.join(os.path.dirname(__file__), 'Code'))
+
 """Main script, uses other modules to generate sentences."""
 from flask import Flask
 from dictogram import Dictogram
@@ -23,7 +28,6 @@ def home():
         return ' '.join(sentence).capitalize() + '.'
     
     return generate_sentence(50)
-
 
 if __name__ == "__main__":
     """To run the Flask server, execute `python app.py` in your terminal.
